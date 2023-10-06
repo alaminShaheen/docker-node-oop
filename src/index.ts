404e6ded3cbefe0f5ad2c2cb27129ea9eb1f6585
alaminShaheen <As2022!!@@>
2023-10-06 11:46:26 -0400

    Added docker and updated some configs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world")
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/", (request: Request, response: Response) => {
+	console.log("Hello world!!!");
+	response.json({ hello: "new" });
+});
+
+app.get("/yo", (request: Request, response: Response) => {
+	response.json({ hello: "yo mother" });
+});
+
+const port  = Number(process.env.PORT || 3000)
+
+app.listen(port, '0.0.0.0', () => {
+	console.log(`Server listening on http://localhost:${port}`);
+})
